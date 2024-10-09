06bd9583fff3bb157c9d397b8925e71080eb011d
s0lena <solena.work@gmail.com>
2024-10-10 00:38:56 +0300

    Bullets 1-8

---
+++ app/(discussion)/topics/[topicId]/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Button from "../../../../components/button/Button";
+import { capitalize, fetchPostByTopic, Post } from "../../../../lib/utils";
+
+export default function TopicPage({ params }: { params: { topicId: string } }) {
+  const topicId = params.topicId;
+  const [topicPosts, setTopicPosts] = useState<Post[]>([]);
+  useEffect(() => {
+    fetchPostByTopic(topicId).then((fetchedPosts) => {
+      setTopicPosts(fetchedPosts);
+    });
+  }, [topicId]);
+  return (
+    <div>
+      <h1>{capitalize(topicId)}</h1>
+      <ul>
+        {topicPosts.map((post) => (
+          <li key={post.id}>
+            <Button href={`/questions/${post.id}`} label={post.title}></Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
